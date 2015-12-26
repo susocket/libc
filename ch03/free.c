8cb414e900c0b09f55d29c3005473619c953ea85
sun <sun@ubuntu.(none)>
2015-12-26 21:27:36 +0800

    ch06 init

--- ch03/free.c
+++
@@ -1,8 +0,0 @@
-/**
- * void free(void *ptr);
- * 参数ptr为指向由malloc()、calloc()或realloc()所返回的内存指针。
- * 调用free()后，ptr所指向的内存空间便会被回收。
- * 假若参数ptr所指项的内存空间已被回收，或是未知的内存地址，则调用free()
- * 可能会有无法预期的情况发生。若参数ptr为NULL，则free不会有作用。
- *
- **/
--- ch04/settimeofday.c
+++
@@ -1,5 +0,0 @@
-/**
- * int settimeofday(const struct timeval *tv, const struct timezone *tz);
- * settimeofday()会把当前时间设置成由tv所指的结构信息，当地时区的信息则设置tz所指的结构。
- * 只有root权限才能使用此函数修改时间。
- **/
