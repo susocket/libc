5eb189124d568b1a75d860c53e8110dc02711e31
susocket@sina.com <susocket@sina.com>
2015-12-29 10:58:38 +0800

    ch08 init

--- ch07/setgrent.c
+++
@@ -1,5 +0,0 @@
-/**
- * void setgrent(void);
- * 用来将getgrent()的读写地址指针指回到文件开头。
- *
- **/
--- ch07/setgroups.c
+++
@@ -1,6 +0,0 @@
-/**
- * int setgroups(size_t size, const gid_t *list);
- * 用来将list数组中所标明的组加入到目前进程的组设置中。
- *
- **/
-
